d1fdaed78d79f03c537b9a77938c710b907bc173
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-07-04 12:23:42 +0530

    Update 03. Hash Tables : Hash functions, sets and map.py

--- 03. Hash Tables : Hash functions, sets and map.py
+++ 03. Hash Tables : Hash functions, sets and map.py
@@ -23,3 +23,38 @@ str1 = 'aaaaaabbbbccccccdd'
 sett = set(str1) # Set Construction O(N) : N is length of string
 # Will get only the unique things
 print(sett)
+
+
+print("\n")
+# Hashmap : Dictionaries
+print("HASHMAP's")
+d = {'code':1, 'scooby':2, 'harry':3}
+print(d)
+
+# Add key:value pair in d: O(1)
+d['arsh'] = 4
+print(d)
+
+# Check for presence of key in dict
+if 'harry' in d:
+    print(True)
+else:
+    print(False)
+
+# Check for value corrosponding to the key: O(1)
+print(d['scooby'])
+
+# traverse the hashmap
+for key,val in d.items():
+    print(f"{key}->{val}")
+
+# default dict
+from collections import defaultdict
+default = defaultdict(int)
+print(f"Default value 0 for all index: {default[2]}")
+
+# Counter
+from collections import Counter
+print(str1)
+count = Counter(str1)
+print(count)
