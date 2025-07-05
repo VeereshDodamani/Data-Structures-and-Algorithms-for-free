3d720f5a9f29a0aab74dc62601ed3618b7d47398
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-07-06 00:07:27 +0530

    Update 04. Stacks and Queues.py

--- 04. Stacks and Queues.py
+++ 04. Stacks and Queues.py
@@ -1,5 +1,5 @@
 # Stacks: Last In First Out
-
+print("STACK")
 stk = []
 print(f"Initial stack: {stk}")
 
@@ -21,3 +21,28 @@ if stk:
     print(f"Stack elements: {stk}")
 else:
     print(f"Empty stack: {stk}")
+print("\n")
+
+# Queues: First In First Out
+print("QUEUES")
+from collections import deque
+q = deque()
+# deque: Double ended queue
+print(q)
+
+# Enqueue a element on the right: O(1)
+q.append(5)
+q.append(6)
+q.append(7)
+q.append(8)
+print(f"After Enqueuing: {q}")
+
+# Dequeue a element from left: O(1)
+q.popleft()
+print(f"After Dequeuing: {q}")
+
+# Peek from left side: O(1)
+print(f"Leftmost element: {q[0]}")
+
+# Peek from right side: O(1)
+print(f"Leftmost element: {q[-1]}")
