724a22fea1d27853050837010696520bc384ea8c
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-07-08 00:06:06 +0530

    Update 05. Recursion.py

--- 05. Recursion.py
+++ 05. Recursion.py
@@ -1,1 +1,17 @@
+# Fibonacci
+# F(0) = 0
+# F(1) = 1
+# F(2) = 2
+# n>1; F(n)=(n-1)+(n-2)
 
+# Time: O(2^n)
+# Space: O(n)
+def F(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return F(n-1) +F(n-2)
+
+print(F(5))
