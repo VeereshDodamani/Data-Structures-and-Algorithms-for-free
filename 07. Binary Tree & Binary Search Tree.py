15faeb92fe771032ee9ea5a9c6f79bdba56708f3
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-07-09 09:40:29 +0530

    Update 07. Binary Tree & Binary Search Tree.py

--- 07. Binary Tree & Binary Search Tree.py
+++ 07. Binary Tree & Binary Search Tree.py
@@ -1,1 +1,66 @@
+# Binary Tree
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+    def __str__(self):
+        return str(self.val)
 
+
+A = TreeNode(1)
+B = TreeNode(2)
+C = TreeNode(3)
+D = TreeNode(4)
+E = TreeNode(5)
+F = TreeNode(10)
+
+A.left = B
+A.right = C
+B.left = D
+B.right = E
+C.left = F
+
+
+# Recursive Pre-order traversal (DFS)
+# Time: O(n)
+# Space: O(n)
+print("Recursive Pre-order traversal")
+def pre_order(node):
+    if not node:
+        return
+    
+    print(node)
+    pre_order(node.left)
+    pre_order(node.right)
+
+pre_order(A)
+
+# Recursive In-order traversal (DFS)
+# Time: O(n)
+# Space: O(n)
+print("Recursive In-order traversal")
+def pre_order(node):
+    if not node:
+        return
+    
+    pre_order(node.left)
+    print(node)
+    pre_order(node.right)
+
+pre_order(A)
+
+# Recursive Post-order traversal (DFS)
+# Time: O(n)
+# Space: O(n)
+print("Recursive Post-order traversal")
+def pre_order(node):
+    if not node:
+        return
+    
+    pre_order(node.left)
+    pre_order(node.right)
+    print(node)
+
+pre_order(A)
