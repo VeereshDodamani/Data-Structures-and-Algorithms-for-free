687ede748fa30e977d0adc4a2105e26ba5aaf20d
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-07-09 10:08:10 +0530

    Update 07. Binary Tree & Binary Search Tree.py

--- 07. Binary Tree & Binary Search Tree.py
+++ 07. Binary Tree & Binary Search Tree.py
@@ -41,26 +41,70 @@ pre_order(A)
 # Time: O(n)
 # Space: O(n)
 print("Recursive In-order traversal")
-def pre_order(node):
+def in_order(node):
     if not node:
         return
     
-    pre_order(node.left)
+    in_order(node.left)
     print(node)
-    pre_order(node.right)
+    in_order(node.right)
 
-pre_order(A)
+in_order(A)
 
 # Recursive Post-order traversal (DFS)
 # Time: O(n)
 # Space: O(n)
 print("Recursive Post-order traversal")
-def pre_order(node):
+def post_order(node):
     if not node:
         return
     
-    pre_order(node.left)
-    pre_order(node.right)
+    post_order(node.left)
+    post_order(node.right)
     print(node)
 
-pre_order(A)
+post_order(A)
+
+# Itreative Post-order/In-order/Post-order traversal (DFS)
+print("Itreative Post-order traversal")
+def iterative_pre_order(node):
+    stk = [node]
+
+    while stk:
+        node = stk.pop()
+        print(node)
+        if node.right:
+            stk.append(node.right)
+        if node.left:
+            stk.append(node.left)
+iterative_pre_order(A)
+
+# Level order traversal (BFS)
+print("Level order traversal")
+from collections import deque
+
+def level_order(node):
+    q = deque()
+    q.append(node)
+    while q:
+        node = q.popleft()
+        print(node)
+        if node.left:
+            q.append(node.left)
+        if node.right:
+            q.append(node.right)
+level_order(A)
+
+# Search for a value (DFS)
+# Time: O(n)
+# Value: O(n)
+print("Searching a node using DFS")
+def search(node, target):
+    if not node:
+        return False
+    if node.val == target:
+        return f"Target: {target} present in tree"
+    
+    return search(node.left, target) or search(node.right, target)
+
+print(search(A,10))
