e95ce79bf208a1e40e31fb50d95549bab1a7e61c
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-07-12 20:22:24 +0530

    Create Heaps & Priority Queues.py

---
+++ Heaps & Priority Queues.py
@@ -0,0 +1,7 @@
+# Heap
+
+# Min-Heap: The parent is smaller than children
+# Max-Heap: The parent is greater than children
+# Heap-pop: Removing a node, O(log n)
+# Heap-insert or Heap-push: Adding a element, O(log n)
+# Heap-sort: O(n log n)
