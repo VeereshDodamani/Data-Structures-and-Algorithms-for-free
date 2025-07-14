5ea8954c28e1cd35c9703184d304d0daec70af18
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-07-14 09:29:09 +0530

    Update 09. Sorting Algorithms.py

--- 09. Sorting Algorithms.py
+++ 09. Sorting Algorithms.py
@@ -57,3 +57,53 @@ def selection_sort(c_arr):
 selection_sort(C)
 print(f"After selection sort: {C}")
 print("\n")
+
+## Merge Sort
+# Time: O(nlogn)
+# Space: O(n)
+
+D = [-5,9,4,-2,7,-3,1,0]
+print(f"Before Merge sort{D}")
+def merge_sort(d_arr):
+    n = len(d_arr)
+    if n == 1:
+        return d_arr
+    
+    m = n // 2
+    L = d_arr[:m]
+    R = d_arr[m:]
+
+    L = merge_sort(L)
+    R = merge_sort(R)
+
+    l, r = 0, 0
+    L_len = len(L)
+    R_len = len(R)
+
+    sorted_arr = [0] * n
+    i = 0
+
+    while l < L_len and r < R_len:
+        if L[l] < R[r]:
+            sorted_arr[i] = L[l]
+            l += 1
+        else:
+            sorted_arr[i] = R[r]
+            r += 1
+        i += 1
+
+    while l < L_len:
+        sorted_arr[i] = L[l]
+        l += 1
+        i += 1
+
+    while r < R_len:
+        sorted_arr[i] = R[r]
+        r += 1
+        i += 1
+
+    print(f"After Merge sort{sorted_arr}")
+    return sorted_arr
+
+merge_sort(D)
+print("\n")
