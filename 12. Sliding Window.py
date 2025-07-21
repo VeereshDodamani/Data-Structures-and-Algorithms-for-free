05dcd6d84e05a339546ae5b6a7cfeba597750e54
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-07-21 19:16:42 +0530

    Update 12. Sliding Window.py

--- 12. Sliding Window.py
+++ 12. Sliding Window.py
@@ -3,6 +3,7 @@
 # 2. Fixed length
 
 # Variable length sliding window
+# Leetcode question: 3 
 s = "abcabcbb"
 l = 0
 longest = 0
@@ -18,4 +19,26 @@ for r in range(n):
     longest = max(longest, w)
     sett.add(s[r])
 
-print(longest)
+print(f"Longest: {longest}")
+
+# Fixed length sliding window
+# Leetcode question: 643
+nums = [1,12,-5,-6,50,3]
+k = 4
+
+n = len(nums)
+cur_sum = 0
+
+for i in range(k):
+    cur_sum += nums[i]
+
+max_avg = cur_sum / k
+
+for i in range(k,n):
+    cur_sum += nums[i]
+    cur_sum -= nums[i-k]
+
+    avg = cur_sum / k
+    max_avg = max(max_avg, avg)
+
+print(f"Max-avg: {max_avg}")
