f92fe1c89eed648f7085a914017cff12f900afb1
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-07-24 21:11:40 +0530

    Update 14. Dynamic Programming.py

--- 14. Dynamic Programming.py
+++ 14. Dynamic Programming.py
@@ -10,4 +10,18 @@ def fib(n):
         
     return fib(n-2)+fib(n-1)
 
-print(fib(4))
+n = int(input("Enter number for Fibonacci series: "))
+print(f"The {n}th Fibonacci number is: {fib(n)}")
+
+
+print("Way-2")
+memo = {0: 0, 1: 1}
+
+def f(x):
+    if x in memo:
+        return memo[x]
+    memo[x] = f(x - 1) + f(x - 2)
+    return memo[x]
+
+n = int(input("Enter number for Fibonacci series: "))
+print(f"The {n}th Fibonacci number is: {f(n)}")
