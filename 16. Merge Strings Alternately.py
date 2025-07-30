b3f58261fcc5e35ce34196158a253bf9d5f588e3
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-07-30 08:40:25 +0530

    Update 16. Merge Strings Alternately.py

--- 16. Merge Strings Alternately.py
+++ 16. Merge Strings Alternately.py
@@ -1,2 +1,29 @@
 ## Merge Strings Alternately
 # Leetcode Problem: 1768
+
+word1 = "abc"
+word2 = "pqrs"
+
+A, B = len(word1), len(word2)
+a, b = 0,0
+s = []
+
+word = 1
+while a<A and b<B:
+    if word == 1:
+        s.append(word1[a])
+        a += 1
+        word = 2
+    else:
+        s.append(word2[b])
+        b += 1
+        word = 1
+
+while a<A:
+    s.append(word1[a])
+    a += 1
+while b<B:
+    s.append(word2[b])
+    b += 1
+
+print("".join(s))
