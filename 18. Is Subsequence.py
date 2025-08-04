5eb4ac7d82df472dca03266785794eef00321279
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-04 07:43:25 +0530

    Update 18. Is Subsequence.py

--- 18. Is Subsequence.py
+++ 18. Is Subsequence.py
@@ -1,1 +1,26 @@
+## Is Subsequence
+# Leetcode problem: 3
 
+s = "abc"
+t = "ahbgdc"
+
+S = len(s)
+T = len(t)
+
+if s == '':
+    print(True)
+    exit()
+
+if S > T:
+    print(False)
+    exit()
+
+j = 0
+for i in range(T):
+    if t[i] == s[j]:
+        j += 1
+        if j == S:
+            print(True)
+            break
+else:
+    print(False)
