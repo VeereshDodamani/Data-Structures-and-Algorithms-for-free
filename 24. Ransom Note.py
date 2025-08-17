47957f892a103a8ec06412fbcea6490883cd7b92
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-17 11:38:51 +0530

    Update 24. Ransom Note.py

--- 24. Ransom Note.py
+++ 24. Ransom Note.py
@@ -1,7 +1,6 @@
 # Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
-# Each letter in magazine can only be used once in ransomNote.
-# Leetcode rpoblem: 383
 
+# Each letter in magazine can only be used once in ransomNote.
 
 print("Way-1")
 ransomNote = "aa"
@@ -14,3 +13,25 @@ def can(magazine,ransomNote):
         return False
 
 print(can(magazine,ransomNote))
+
+print("Way-2")
+ransomNote = "aab"
+magazine = "baa"
+def can2(ransomNote,magazine):
+    counter = {}
+    for c in magazine:
+        if c in counter:
+            counter[c] += 1
+        else:
+            counter[c] = 1
+
+    for c in ransomNote:
+        if c not in counter:
+            return False
+        elif counter[c] == 1:
+            del counter[c]
+        else:
+            counter[c] -= 1
+    return True
+
+print(can2(ransomNote,magazine))
