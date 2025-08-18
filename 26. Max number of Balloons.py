04ea1c90c691637102756c7b97a9634662c57503
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-18 12:58:28 +0530

    Update 26. Max number of Balloons.py

--- 26. Max number of Balloons.py
+++ 26. Max number of Balloons.py
@@ -1,3 +1,7 @@
+# Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+# You can use each character in text at most once. Return the maximum number of instances that can be formed.
+# Leetcode problem: 1189
+
 from collections import defaultdict
 
 class Solution:
