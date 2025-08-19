9e53fe905acce661950d39ca3ae7c12a4d062c41
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-19 20:59:28 +0530

    Update 28. Majority Element.py

--- 28. Majority Element.py
+++ 28. Majority Element.py
@@ -1,7 +1,8 @@
 # Given an array nums of size n, return the majority element.
-
 # The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+# Leetcode problem: 169
 
+print("Way-1")
 def majorityElement(nums):
     count = 0
     candidate = None
@@ -15,5 +16,20 @@ def majorityElement(nums):
             count -= 1
     return candidate
 
-nums = [1,2,1,2,2,1,2,2,2,1]
-print(majorityElement(nums))
+nums = [3,1,2,1,2,2,1,2,2,2,1]
+print(nums)
+print("Majority element from given nums: ",majorityElement(nums))
+print("\n")
+
+print("Way-2")
+mydict = {}
+def majorityElement2(nums):
+    for num in nums:
+        if num not in mydict:
+            mydict[num] = 1
+        else:
+            mydict[num] += 1
+    return max(mydict)
+nums = [3,2,3]
+print(nums)
+print("Majority element from given nums: ",majorityElement2(nums))
