0030f0ec47df0b33978d92218c81a29497ac558b
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-20 18:38:54 +0530

    Create 30. Reverse String.py

---
+++ 30. Reverse String.py
@@ -0,0 +1,13 @@
+# Write a function that reverses a string. The input string is given as an array of characters s.
+# Leetcode problem: 344
+
+print("Way-1")
+s = ["H","a","n","n","a","h"]
+print("Given string: ",s)
+i = len(s)-1
+a = []
+
+while i>=0:
+    a.append(s[i])
+    i -= 1
+print("Reversed string",a)
