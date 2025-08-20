887f66c4a0b57a674a80c656484da679f31bbacd
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-20 18:45:50 +0530

    Update 30. Reverse String.py

--- 30. Reverse String.py
+++ 30. Reverse String.py
@@ -11,3 +11,14 @@ while i>=0:
     a.append(s[i])
     i -= 1
 print("Reversed string",a)
+
+
+print("Way-2")
+def reverseString(s):
+    left, right = 0, len(s)-1
+    while left<right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    return(s)
+print(reverseString(s))
