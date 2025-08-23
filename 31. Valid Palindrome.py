084db5c26dfa449f75729d31c9035fe64eb3111d
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-23 22:45:20 +0530

    Update 31. Valid Palindrome.py

--- 31. Valid Palindrome.py
+++ 31. Valid Palindrome.py
@@ -2,6 +2,8 @@
 
 # Given a string s, return true if it is a palindrome, or false otherwise.
 
+
+print("Way-1")
 s = "A man, a plan, a canal: Panama"
 s = s.lower()
 print(s)
@@ -30,3 +32,29 @@ def pal(s):
         return ("Valid Palindrome: False")
 
 print(pal(s))
+print("\n")
+
+print("Way-2")
+s = "race a car"
+def isPalindrome(s):
+    print(s)
+    n = len(s)
+    L = 0
+    R = n-1
+
+    while L<R:
+        if not s[L].isalnum():
+            L += 1
+            continue
+        if not s[R].isalnum():
+            R -= 1
+            continue
+        
+        if s[L].lower() != s[R].lower():
+            return ("Valid Palindrome: False")
+        
+        L += 1
+        R -= 1
+    return ("Valid Palindrome: True")
+
+print(isPalindrome(s))
