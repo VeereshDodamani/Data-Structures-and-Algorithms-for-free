c81dd2d9486b68fee3ecd42cd24b93c6b14be31d
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-24 18:46:58 +0530

    Update 33. Three sum.py

--- 33. Three sum.py
+++ 33. Three sum.py
@@ -1,6 +1,7 @@
 # Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 # Notice that the solution set must not contain duplicate triplets.
 
+print("Way-1")
 nums = [-1,0,1,2,-1,-4]
 n=len(nums)
 i=0
@@ -10,3 +11,20 @@ for i in range(n):
         for k in range(j+1,n):
             if nums[i]+nums[j]+nums[k] == target:
                 triplet = print(nums[i],nums[j],nums[k])
+
+print("\n")
+print("Way-2")
+def threeSum(nums,target):
+    n = len(nums)
+    found = set()
+    for i in range(n):
+        for j in range(i+1, n):
+            for k in range(j+1, n):
+                if nums[i] + nums[j] + nums[k] == target:
+                    triplet = tuple(sorted([nums[i], nums[j], nums[k]]))
+                    found.add(triplet)
+    return [list(trip) for trip in found]
+
+nums = [-1,0,1,2,-1,-4]
+target = 0
+print(threeSum(nums,target))
