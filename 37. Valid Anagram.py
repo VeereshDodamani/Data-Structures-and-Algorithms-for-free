3709ac9af36a1c243773ef2fc5a7a7bb2a9cbadb
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-08-27 22:36:30 +0530

    Update 37. Valid Anagram.py

--- 37. Valid Anagram.py
+++ 37. Valid Anagram.py
@@ -1,6 +1,23 @@
 # Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
+from collections import Counter
+print("Way-1")
+s = "act"
+t = "cat"
 def is_anagram(s, t):
     return sorted(s) == sorted(t)
 
-print(is_anagram("act", "cat"))
+print(is_anagram(s, t))
+
+
+print("Way-2")
+def is_anagram1(s, t):
+    if len(s) != len(t):
+        return False
+    
+    s_dict = Counter(s)
+    t_dict = Counter(t)
+
+    return s_dict == t_dict
+
+print(is_anagram1(s, t))
