b7aa969f18b10c18fd854ab106b14b7cdfa80c41
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-09-08 08:28:58 +0530

    Update 44. Min Stack.py

--- 44. Min Stack.py
+++ 44. Min Stack.py
@@ -1,3 +1,15 @@
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+# Implement the MinStack class:
+
+# MinStack() initializes the stack object.
+# void push(int val) pushes the element val onto the stack.
+# void pop() removes the element on the top of the stack.
+# int top() gets the top element of the stack.
+# int getMin() retrieves the minimum element in the stack.
+# You must implement a solution with O(1) time complexity for each function.
+
+
 class MinStack:
     def __init__(self):
         self.stack = []
@@ -31,13 +43,13 @@ if __name__ == "__main__":
     min_stack = MinStack()
     min_stack.push(3)
     min_stack.push(5)
-    print("Current min:", min_stack.getMin())  # Output: 3
+    print("Current min:", min_stack.getMin()) 
     min_stack.push(2)
     min_stack.push(1)
-    print("Current min:", min_stack.getMin())  # Output: 1
+    print("Current min:", min_stack.getMin()) 
     min_stack.pop()
-    print("Current min after pop:", min_stack.getMin())  # Output: 2
-    print("Top element:", min_stack.top())  # Output: 2
+    print("Current min after pop:", min_stack.getMin()) 
+    print("Top element:", min_stack.top())
     min_stack.pop()
-    print("Top element after popping:", min_stack.top())  # Output: 5
-    print("Current min:", min_stack.getMin())  # Output: 3
+    print("Top element after popping:", min_stack.top())
+    print("Current min:", min_stack.getMin())
