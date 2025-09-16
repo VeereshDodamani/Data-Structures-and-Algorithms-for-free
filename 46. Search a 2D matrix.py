2f1fa782587d9d8da6a502476081dfe7f7846556
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-09-16 07:24:38 +0530

    Create 46. Search a 2D matrix.py

---
+++ 46. Search a 2D matrix.py
@@ -0,0 +1,28 @@
+def searchMatrix(matrix, target):
+    m = len(matrix)
+    n = len(matrix[0])
+    t = m*n
+    l = 0
+    r = t - 1
+
+    while l<=r:
+        m = (l+r) // 2
+        i = m // n
+        j = m % n 
+
+        mid_num = matrix[i][j]
+
+        if target == mid_num:
+            return True
+        elif target < mid_num:
+            r = m - 1
+        else:
+            l = m + 1
+    
+    return False
+
+
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+target = 3
+
+print(searchMatrix(matrix, target))
