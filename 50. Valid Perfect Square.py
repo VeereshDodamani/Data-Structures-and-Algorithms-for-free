67333a4fe8eb69ce9206b52586ad9456934e70d3
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-09-24 08:05:16 +0530

    Update 50. Valid Perfect Square.py

--- 50. Valid Perfect Square.py
+++ 50. Valid Perfect Square.py
@@ -1,3 +1,9 @@
+# Given a positive integer num, return true if num is a perfect square or false otherwise.
+
+# A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+
+# You must not use any built-in library function, such as sqrt.
+
 def is_perfect_square(n):
     if n < 0:
         return False
