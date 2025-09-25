26053760508111ae68e604ab9a62fe27229d582a
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-09-25 07:14:26 +0530

    Update 51. Koko Eating Banana.py

--- 51. Koko Eating Banana.py
+++ 51. Koko Eating Banana.py
@@ -1,3 +1,12 @@
+# Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+# Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+# Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+# Return the minimum integer k such that she can eat all the bananas within h hours.
+# Leetcode probelm: 875
+
 from math import ceil
 piles = [30,11,23,4,20]
 h = 5
@@ -21,4 +30,4 @@ def minEatingSpeed(piles, h):
     
     return l
 
-print(minEatingSpeed(piles, h))
+print("Minimum hours required: ",minEatingSpeed(piles, h))
