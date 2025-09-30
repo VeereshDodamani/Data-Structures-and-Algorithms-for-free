a6a74ded0a1bbb1a0745a20b4fc16d6403658177
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-09-30 21:18:34 +0530

    Create 53. Removing nth node from last.py

---
+++ 53. Removing nth node from last.py
@@ -0,0 +1,40 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def list_to_linkedlist(lst):
+    dummy = ListNode(0)
+    cur = dummy
+    for val in lst:
+        cur.next = ListNode(val)
+        cur = cur.next
+    return dummy.next
+
+def removeNthFromEnd(head, n):
+    dummy = ListNode(0)
+    dummy.next = head
+    ahead = behind = dummy
+
+    for _ in range(n + 1):
+        ahead = ahead.next
+
+    while ahead:
+        ahead = ahead.next
+        behind = behind.next
+
+    behind.next = behind.next.next
+    return dummy.next
+
+def print_linkedlist(head):
+    cur = head
+    while cur:
+        print(cur.val, end=" ")
+        cur = cur.next
+    print()
+
+head_list = [1, 2, 3, 4, 5]
+n = 2
+head = list_to_linkedlist(head_list)
+new_head = removeNthFromEnd(head, n)
+print_linkedlist(new_head)
