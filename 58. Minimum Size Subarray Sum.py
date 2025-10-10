879e8226a13cfc4eb5d6f4882265211d195baef4
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-10-10 09:53:32 +0530

    Update 58. Minimum Size Subarray Sum.py

--- 58. Minimum Size Subarray Sum.py
+++ 58. Minimum Size Subarray Sum.py
@@ -1,3 +1,6 @@
+# Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+# Leetcode problem: 209
+
 def minSubArrayLen(target, nums):
     min_length = float('inf')
     summ = 0
@@ -19,4 +22,4 @@ print("Target given: ", target)
 nums = [2,3,1,2,4,3]
 print("Array given: ", nums)
 
-print("Minimum sub array is: ",minSubArrayLen(target, nums))
+print("Minimum size sub array is: ",minSubArrayLen(target, nums))
