15229b3b394583792ab7f5e68c3ce494a39f09b4
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-10-13 22:22:38 +0530

    Create 59. Max Avg Subarray.py

---
+++ 59. Max Avg Subarray.py
@@ -0,0 +1,26 @@
+# You are given an integer array nums consisting of n elements, and an integer k.
+
+# Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+def findMaxAverage(nums, k):
+    n = len(nums)
+    cur_sum = 0
+
+    for i in range(k):
+        cur_sum += nums[i]
+
+    max_avg = cur_sum / k
+
+    for i in range(k, n):
+        cur_sum += nums[i]
+        cur_sum -= nums[i]
+
+        avg = cur_sum / k
+        max_avg = max(max_avg, avg)
+
+    return max_avg
+
+nums = [1,12,-5,-6,50,3]
+k = 4
+
+print(findMaxAverage(nums,k))
