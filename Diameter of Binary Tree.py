30af3c6c853f1255c5ea8e2aad28179ea2f8a5c3
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-10-20 08:48:38 +0530

    Update Diameter of Binary Tree.py

--- Diameter of Binary Tree.py
+++ Diameter of Binary Tree.py
@@ -23,3 +23,30 @@ class Solution:
         
         height(root)
         return largest_diameter[0]
+
+
+# VS Code solution
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root):
+        self.res = 0
+        def dfs(node):
+            if not node: return 0
+            l, r = dfs(node.left), dfs(node.right)
+            self.res = max(self.res, l + r)
+            return 1 + max(l, r)
+        dfs(root)
+        return self.res
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+print("Maximum Diameter of Binary Tree: ",Solution().diameterOfBinaryTree(root))
