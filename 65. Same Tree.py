e6d36ba3b81a74506ab066058a9cf28c6cc1af44
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-10-21 17:19:08 +0530

    Create 65. Same Tree.py

---
+++ 65. Same Tree.py
@@ -0,0 +1,28 @@
+# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+# Leetcode probelm: 100
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def balanced(p, q):
+    if not p and not q:
+        return True
+    if not p or not q:
+        return False
+    if p.val != q.val:
+        return False
+    return balanced(p.left, q.left) and balanced(p.right, q.right)
+
+p = Node(1)
+p.left = Node(2)
+p.right = Node(3)
+
+q = Node(1)
+q.left = Node(2)
+q.right = Node(3)
+
+print(balanced(p, q))
