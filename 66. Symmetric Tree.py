692f10ba0ff048eead4904ae6cb094c9c294f22f
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-10-22 08:57:28 +0530

    Create 66. Symmetric Tree.py

---
+++ 66. Symmetric Tree.py
@@ -0,0 +1,21 @@
+# Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+# leetcode problem : 101
+
+# Leetcode solution
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def same(root1, root2):
+            if not root1 and not root2:
+                return True
+            
+            if not root1 or not root2:
+                return False
+
+            if root1.val != root2.val:
+                return False
+
+            return same(root1.left, root2.right) and same(root1.right, root2.left)
+        
+        return same(root, root)
+    
+root = [1,2,2,3,4,4,3]
