86fdc38b72d8499b839b18e1ed00d9c03e223230
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-01 08:56:00 +0530

    Create 71. Avg of Levels in BT.py

---
+++ 71. Avg of Levels in BT.py
@@ -0,0 +1,24 @@
+# Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
+#Leetcode probelm: 367
+
+# WAY 1
+from collections import deque
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        avgs = []
+        q = deque([root])  # No check if root is None
+
+        while q:
+            total = 0
+            n = len(q)
+            for _ in range(n):
+                node = q.popleft()
+                total += node.val
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            avgs.append(total / n)
+        return avgs
