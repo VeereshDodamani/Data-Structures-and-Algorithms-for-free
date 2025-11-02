5df82dac179f61d17d9bd1fe9ad2b4f2d9454c9a
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-02 12:53:13 +0530

    Create 72. Kth smallest element in BST.py

---
+++ 72. Kth smallest element in BST.py
@@ -0,0 +1,22 @@
+# Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+# Leetcode problem: 230
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        count = [k]
+        ans = [0]
+
+        def dfs(node):
+            if not node:
+                return
+            
+            dfs(node.left)
+            count[0] -= 1
+
+            if count[0] == 0:
+                ans[0] = node.val
+                return
+            
+            dfs(node.right)
+
+        dfs(root)
+        return ans[0]
