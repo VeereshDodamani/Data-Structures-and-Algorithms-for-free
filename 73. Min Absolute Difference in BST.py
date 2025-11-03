721385f8769f157bad6a7e99a5766fb639029bf0
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-03 10:44:23 +0530

    Create 73. Min Absolute Difference in BST.py

---
+++ 73. Min Absolute Difference in BST.py
@@ -0,0 +1,21 @@
+# Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+# Leetcode problem: 530
+
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        min_distance = [float('inf')]
+        prev = [None]
+
+        def dfs(node):
+            if node is None:
+                return
+
+            dfs(node.left)
+            if prev[0] is not None:
+                min_distance[0] = min(min_distance[0], node.val - prev[0])
+            prev[0] = node.val
+
+            dfs(node.right)
+
+        dfs(root)
+        return min_distance[0]
