f5881ed19d64400f2453e679aa3fd300b7a052dc
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-15 13:43:41 +0530

    Update 82. Permulations.py

--- 82. Permulations.py
+++ 82. Permulations.py
@@ -1,3 +1,6 @@
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+# Leetcode probelm: 46
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
