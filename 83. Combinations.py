0603e5f02f9ba5acab647222a229aed9d8ea0086
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-16 08:34:03 +0530

    Update 83. Combinations.py

--- 83. Combinations.py
+++ 83. Combinations.py
@@ -1,3 +1,7 @@
+# Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+# You may return the answer in any order.
+# Leetcode probelm: 77
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         ans, sol = [], []
