978aaac70a7b9da4bdfa533dde9674f6f003a264
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-29 23:59:11 +0530

    Create 87. Word Search.py

---
+++ 87. Word Search.py
@@ -0,0 +1,32 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m = len(board)
+        n = len(board[0])
+        w = len(word)
+
+        def backtrack(pos, index):
+            i, j = pos
+            if index == w:
+                return True
+
+            if board[i][j] != word[index]:
+                return False
+
+            char = board[i][j]
+            board[i][j] = '#'
+
+            for i_off, j_off in [(0,1),(1,0),(0,-1),(-1,0)]:
+                r, c = i + i_off, j + j_off
+                if 0 <= r < m and 0 <= c < n and board[r][c] != '#':
+                    if backtrack((r, c), index + 1):
+                        board[i][j] = char
+                        return True
+
+            board[i][j] = char
+            return False
+
+        for i in range(m):
+            for j in range(n):
+                if backtrack((i, j), 0):
+                    return True
+        return False
