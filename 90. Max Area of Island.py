02807db4ced15901dc0e0b59a7cb65aed81881e0
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-12-10 23:04:53 +0530

    Update 90. Max Area of Island.py

--- 90. Max Area of Island.py
+++ 90. Max Area of Island.py
@@ -4,14 +4,21 @@ class Solution:
         n = len(grid[0])
 
         def dfs(i, j):
-            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != '1':
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != 1:
                 return 0
             grid[i][j] = 0
-            return 1 + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i - 1, j) + dfs(i, j + 1)
+            return (
+                1
+                + dfs(i + 1, j)
+                + dfs(i, j - 1)
+                + dfs(i - 1, j)
+                + dfs(i, j + 1)
+            )
 
         max_area = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     max_area = max(max_area, dfs(i, j))
+
         return max_area
